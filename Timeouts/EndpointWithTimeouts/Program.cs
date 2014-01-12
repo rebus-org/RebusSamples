4c06a9b4601c2f60c13405fed9298f3765cd84e7
Mogens Heller Grabe <mookid8000@gmail.com>
2014-01-12 19:37:25 +0100

    started working on the sample

---
+++ Timeouts/EndpointWithTimeouts/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Rebus.Configuration;
+using Rebus.Logging;
+using Rebus.Transports.Msmq;
+
+namespace EndpointWithTimeouts
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var adapter = new BuiltinContainerAdapter())
+            {
+                adapter.Handle<DeferredMessage>(msg => Console.WriteLine("Got message sent {0} - current time: {1}", msg.Sent, DateTime.Now));
+
+                Configure.With(adapter)
+                    .Logging(l => l.ColoredConsole(minLevel:LogLevel.Warn))
+                    .Transport(t => t.UseMsmq("test_input", "error"))
+                    .CreateBus()
+                    .Start();
+
+                while (true)
+                {
+                    Console.WriteLine("Press 0...9 to send message n seconds into the future or Q to quit");
+                    
+                    var key = Console.ReadKey(true);
+
+                    switch (key.KeyChar)
+                    {
+                        case '0':
+                        case '1':
+                        case '2':
+                        case '3':
+                        case '4':
+                        case '5':
+                        case '6':
+                        case '7':
+                        case '8':
+                        case '9':
+                            var seconds = int.Parse(key.KeyChar.ToString());
+                            var delay = TimeSpan.FromSeconds(seconds);
+                            Console.WriteLine("Sending message deferred by {0}", delay);
+                            adapter.Bus.Defer(delay, new DeferredMessage {Sent = DateTime.Now});
+                            break;
+
+                        case 'q':
+                        case 'Q':
+                            goto this_is_the_end; //< omg omg omg!!!!1
+                    }
+                }
+
+            this_is_the_end:
+                Console.WriteLine("Quitting...");
+
+            }
+        }
+
+        class DeferredMessage
+        {
+            public DateTime Sent { get; set; }
+        }
+    }
+}
---
+++ Timeouts/README.md
@@ -0,0 +1,26 @@
+## Timeouts
+
+The purpose of this sample is to demonstrate how Rebus can easily _send messages into the future!_
+
+Please note that for the sample to work, you must have a Rebus Timeout Manager running that this program can send deferred messages to.
+
+### How to start the timeout manager?
+
+#### Make sure timeout can be properly persisted
+
+The timeout manager needs some kind of persistent storage for timeouts - this is how your timeouts will never be forgotten, even though your machine might reboot in the middle of doing important work.
+
+Go to the 'Rebus Timeout Manager' folder and open up `Rebus.Timeout.exe.config` in your favorite text editor - it should have a `timeout` element that looks somewhat like this:
+
+	<timeout inputQueue="rebus_timeouts" errorQueue="error" storageType="SQL" 
+			 connectionString="server=.;initial catalog=rebus_test;integrated security=sspi" tableName="test_timeouts" />
+
+which configures the timeout manager to use the local `rebus_test` database to store timeouts - the table `test_timeouts` will automatically be created when the timeout manager starts up.
+
+
+#### Make sure the timeout manager has the right input queue
+
+Take a look at the `timeout` element again - the `inputQueue` property configures the name of the timeout manager's input queue. Pick whatever suits you, just make sure that you remember the name.
+
+
+#### Make sure your Rebus endpoint is configured to send timeout requests to the timeout manager
