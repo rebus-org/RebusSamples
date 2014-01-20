ae2af3a975c1e3bc43ac3a02ec9d8f01c9ccf96d
Mogens Heller Grabe <mookid8000@gmail.com>
2014-01-20 09:23:27 +0100

    added MSMQ scaleout sample

---
+++ MsmqScaleout/Distributor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Rebus.Logging;
+using Rebus.MsmqLoadBalancer;
+
+namespace Distributor
+{
+    class Program
+    {
+        static void Main()
+        {
+            RebusLoggerFactory.Current = new ConsoleLoggerFactory(false) { MinLevel = LogLevel.Warn };
+
+            var loadBalancer = new LoadBalancerService("distributor")
+                .AddDestinationQueue("consumer1")
+                .AddDestinationQueue("consumer2");
+
+            using (loadBalancer.Start())
+            {
+                Console.WriteLine("Rebus load balancer is running - press ENTER to quit");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ MsmqScaleout/Producer/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Messages;
+using Rebus;
+using Rebus.Configuration;
+using Rebus.Logging;
+using Rebus.Transports.Msmq;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main()
+        {
+            using (var adapter = new BuiltinContainerAdapter())
+            {
+                Configure.With(adapter)
+                    .Logging(l => l.ColoredConsole(LogLevel.Warn))
+                    .MessageOwnership(o => o.FromRebusConfigurationSection())
+                    .Transport(t => t.UseMsmqInOneWayClientMode())
+                    .CreateBus()
+                    .Start();
+
+                var keepRunning = true;
+                
+                while (keepRunning)
+                {
+                    Console.WriteLine(@"a) Send 10 jobs
+b) Send 100 jobs
+c) Send 1000 jobs
+
+q) Quit");
+                    var key = char.ToLower(Console.ReadKey(true).KeyChar);
+
+                    switch (key)
+                    {
+                        case 'a':
+                            Send(10, adapter.Bus);
+                            break;
+                        case 'b':
+                            Send(100, adapter.Bus);
+                            break;
+                        case 'c':
+                            Send(1000, adapter.Bus);
+                            break;
+                        case 'q':
+                            Console.WriteLine("Quitting");
+                            keepRunning = false;
+                            break;
+                    }
+                }
+            }
+        }
+
+        static void Send(int numberOfJobs, IBus bus)
+        {
+            Console.WriteLine("Publishing {0} jobs", numberOfJobs);
+
+            var jobs = Enumerable.Range(0, numberOfJobs)
+                .Select(i => new Job { JobNumber = i });
+
+            Parallel.ForEach(jobs, bus.Send);
+        }
+    }
+}
