a5a86a5057ae9f6ceccb7084e631754a04bf83b9
Mogens Heller Grabe <mookid8000@gmail.com>
2014-05-09 23:10:26 +0200

    updated readme

--- MessageBus/README.md
+++ MessageBus/README.md
@@ -1,3 +1,17 @@
 ## MessageBus
 
-This sample is NOT your typical .NET service bus sample  - it's a POC that three independent endpoints can pub/sub the same messages without knowing of each other's existence beforehand, using only a shared central subscription storage in order to register for messages of a particular type.
+This sample is NOT your typical .NET service bus sample  - it's a POC that three independent endpoints 
+can pub/sub the same messages without knowing of each other's existence beforehand, using only a shared 
+central subscription storage in order to register for messages of a particular type.
+
+A single centralized subscription storage is provided by the XML file subscription storage, which will 
+use a file beneath the current user's local app data to store subscriptions - e.g. something like 
+`C:\Users\WhateverYourNameIs\AppData\Local\RebusSamples\MessageBus\subscriptions.xml`.
+
+Each endpoint can subscribe/unsubscribe independently by doing a `bus.Subscribe<string>()` which
+underneath the covers sends a subscription request to the owner of `System.String`. Each endpoint
+believes that it is the owner of `System.String`, which will cause itself to receive the subscription
+request, thereby persisting the subscription in the XML file.
+
+Whenever an endpoint publishes a `System.String`, all the current subscribers will get a copy of
+the message.
--- MessageBus/Shared/SharedCodez.cs
+++ MessageBus/Shared/SharedCodez.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 using Rebus.Configuration;
 using Rebus.Logging;
@@ -8,21 +9,24 @@ namespace Shared
 {
     public class SharedCodez
     {
-        readonly string _endpointName = Assembly.GetEntryAssembly().GetName().Name;
 
         public void Run()
         {
+            var endpointName = Assembly.GetEntryAssembly().GetName().Name;
+            var directory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "RebusSamples", "MessageBus");
+            var subscriptionsFilePath = Path.Combine(directory, "subscriptions.xml");
+
             using (var adapter = new BuiltinContainerAdapter())
             {
                 adapter.Handle<string>(str => Console.WriteLine("Got message: {0}", str));
 
-                Console.WriteLine("Starting bus in {0}...", _endpointName);
-
+                Console.WriteLine("Starting {0} bus - using subscription storage in {1}...", endpointName, subscriptionsFilePath);
+                
                 Configure.With(adapter)
                     .Logging(l => l.ColoredConsole(minLevel: LogLevel.Warn))
                     .Transport(t => t.UseMsmqAndGetInputQueueNameFromAppConfig())
                     .MessageOwnership(o => o.FromRebusConfigurationSection())
-                    .Subscriptions(s => s.StoreInXmlFile(@"C:\temp\subs.xml"))
+                    .Subscriptions(s => s.StoreInXmlFile(subscriptionsFilePath))
                     .CreateBus()
                     .Start();
 
@@ -54,7 +58,7 @@ Q) Quit
 
                         case 'c':
                             Console.WriteLine("Publishing!");
-                            adapter.Bus.Publish(string.Format("Greetings to subscribers from {0}", _endpointName));
+                            adapter.Bus.Publish(string.Format("Greetings to subscribers from {0}", endpointName));
                             break;
 
                         case 'q':
