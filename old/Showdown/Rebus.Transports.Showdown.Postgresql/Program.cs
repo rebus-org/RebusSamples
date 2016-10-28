e14d2b922e58a4e560351d58a04cfb54c9baebb1
James Kelly <james.kelly.99@gmail.com>
2016-10-28 12:55:52 +1100

    add postgresql transport to showdown

---
+++ old/Showdown/Rebus.Transports.Showdown.Postgresql/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+using Rebus.Config;
+using Rebus.PostgreSql.Transport;
+using Rebus.Transports.Showdown.Core;
+
+namespace Rebus.Transports.Showdown.PostgreSql
+{
+    public class Program
+    {
+
+        const string QueueName = "test_showdown";
+        private const string TableName = QueueName;
+        const string PostgresqlConnectionString = "server=localhost;port=5433;database=rebus2_test;user id=test; password=test; Maximum Pool Size=30";
+
+
+        public static void Main()
+        {
+            using (var runner = new ShowdownRunner())
+            {
+                PurgeInputQueue(QueueName);
+
+                Configure.With(runner.Adapter)
+                    .Logging(l => l.None())
+                    .Transport(t => t.UsePostgreSql(PostgresqlConnectionString, TableName, QueueName))
+                    .Start();
+
+            
+
+
+                runner.Run(typeof(Program).Namespace).Wait();
+            }
+        }
+
+        static void PurgeInputQueue(string queueName)
+        {
+
+        }
+    }
+}
