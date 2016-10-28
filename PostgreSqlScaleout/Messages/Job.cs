a52ca697c150b0e1f32adfc48fe5d582b10f5508
James Kelly <james.kelly.99@gmail.com>
2016-10-28 15:19:08 +1100

    create a PostgreSQL version of the SqlScaleout example

---
+++ PostgreSqlScaleout/Messages/Job.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Messages
+{
+    public class Job
+    {
+        public Job(int jobNumber, Guid jobUuid)
+        {
+            JobNumber = jobNumber;
+            JobUuid = jobUuid;
+        }
+
+        public int JobNumber { get; }
+        public Guid JobUuid { get; }
+    }
+}
