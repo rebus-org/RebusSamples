32c93978bd720716ca23380194853f9c081412e5
James Kelly <james.kelly.99@gmail.com>
2016-10-28 12:57:30 +1100

    add new posgresql transport runner to the mix

--- old/Showdown/Rebus.Transports.Showdown.RunAll/Program.cs
+++ old/Showdown/Rebus.Transports.Showdown.RunAll/Program.cs
@@ -1,12 +1,17 @@
-﻿namespace Rebus.Transports.Showdown.RunAll
+﻿using System;
+
+namespace Rebus.Transports.Showdown.RunAll
 {
     class Program
     {
         static void Main()
         {
-            //SqlServer.Program.Main();
-            Showndown.Msmq.Program.Main();
-            //Showndown.RabbitMq.Program.Main();
+            SqlServer.Program.Main();
+            PostgreSql.Program.Main(); 
+            Msmq.Program.Main();
+            RabbitMq.Program.Main(); 
+            Console.WriteLine("Showdown complete, press any key to continue....");
+            Console.ReadKey();
         }
     }
 }
